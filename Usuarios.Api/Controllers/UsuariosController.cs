9eb99550b722648cbbf1e110b0537283a0fe0370
EverUniAndes <eo.martinez2@uniandes.edu.co>
2025-05-14 23:18:49 -0500

    endpoint para validar el token

--- Usuarios.Api/Controllers/UsuariosController.cs
+++ Usuarios.Api/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Usuarios.Api.Helpers;
 using Usuarios.Aplicacion.Comun;
 using Usuarios.Aplicacion.Usuario.Comandos;
 using Usuarios.Aplicacion.Usuario.Dto;
@@ -82,6 +83,36 @@ namespace Usuarios.Api.Controllers
             {
                 return Problem(output.Mensaje, statusCode: (int)output.Status);
             }
+        }
+
+        /// <summary>
+        /// Autoriza el acceso a una aplicación
+        /// </summary>
+        /// <param name="aplicacion">Identificador de la aplicación: 1 - web, 2 - appmovil, 3 - Api interna</param>
+        /// <response code="200"> 
+        /// TokenInfo: objeto de salida <br/>
+        /// Resultado: Enumerador de la operación, Exitoso = 1, Error = 2, SinRegistros = 3 <br/>
+        /// Mensaje: Mensaje de la operación <br/>
+        /// Status: Código de estado HTTP <br/>
+        /// </response>
+        [HttpGet]
+        [Route("Autorizar/{aplicacion}")]
+        [ProducesResponseType(typeof(TokenInfo), 200)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [Authorize]
+        public IActionResult Autorizar([FromRoute] int aplicacion)
+        {
+            var output = new TokenInfo
+            {
+                Username = HttpContext.Items["UserId"].ToString(),
+                Mensaje = "Operación exitosa",
+                Resultado = Resultado.Exitoso,
+                Status = System.Net.HttpStatusCode.OK
+            };
+            
+            return Ok(output);
+            
         }
 
     }
